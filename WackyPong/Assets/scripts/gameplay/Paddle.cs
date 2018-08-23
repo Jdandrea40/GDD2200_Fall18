b42181e289a3a0d06ac1bc455b31fdd4b32448fc
Jdandrea40 <jdandrea40@yahoo.com>
2018-08-23 12:40:09 -0600

    Initial Commit

---
+++ WackyPong/Assets/scripts/gameplay/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A paddle
+/// </summary>
+public class Paddle : MonoBehaviour
+{
+	
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+		
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+		
+	}
+	
+}
