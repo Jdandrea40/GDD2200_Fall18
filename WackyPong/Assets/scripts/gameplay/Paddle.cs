71ec57f4364c62927f5b6a0831216580c66dc325
Jdandrea40 <jdandrea40@yahoo.com>
2018-08-27 15:44:17 -0600

    push 1 - pi 1
    - paddle testing - doesn't work

--- WackyPong/Assets/scripts/gameplay/Paddle.cs
+++ WackyPong/Assets/scripts/gameplay/Paddle.cs
@@ -7,19 +7,41 @@ using UnityEngine;
 /// </summary>
 public class Paddle : MonoBehaviour
 {
-	
-	/// <summary>
-	/// Use this for initialization
-	/// </summary>
-	void Start()
+    // Saved for efficiency
+    [SerializeField]
+    int paddleSpeed = 5;     // Serialized to allow Inspector Changes
+
+    Rigidbody2D rb2d;
+
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
 	{
-		
+
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
 	}
-	
-	/// <summary>
-	/// Update is called once per frame
-	/// </summary>
-	void Update()
+
+    
+
+    void FixedUpdate()
+    {
+        if(Input.GetAxisRaw("Paddle1") != 0)
+        {
+            transform.Translate(new Vector3(0, Input.GetAxisRaw("Paddle1") * paddleSpeed * Time.deltaTime, 0));
+            Debug.Log("Input registered");
+        }
+        else
+        {
+            paddleSpeed = 0;
+        }
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
 	{
 		
 	}
