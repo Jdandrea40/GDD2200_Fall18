679e744357bcb3fa06b186b1d0c74c80a614425a
Jdandrea40 <jdandrea40@yahoo.com>
2018-08-29 11:52:17 -0600

    Push 4 - PI 1
    + no notable changes

--- WackyPong/Assets/scripts/gameplay/Ball.cs
+++ WackyPong/Assets/scripts/gameplay/Ball.cs
@@ -10,24 +10,29 @@ public class Ball : MonoBehaviour
     // saved for efficiency
     float ballImpulse = ConfigurationUtils.BallImpulseForce;
     Rigidbody2D rb2d;
+    float degree;
 
 	/// <summary>
 	/// Use this for initialization
 	/// </summary>
 	void Start()
 	{
+
         // gets Rigidbody2D Componenet
         rb2d = GetComponent<Rigidbody2D>();
+        degree = Random.Range(135, 225);
+        float radians = degree * Mathf.Deg2Rad;
+        Debug.Log(radians);
 
         //transform.position = new Vector3(Random.Range(135, 225), transform.position.y, transform.position.z);
         //Vector2 direction = transform.forward;
         //direction = new Vector2(1, direction.y);
-        //rb2d.AddForce(direction * ballImpulse);
+        //rb2d.AddForce(new Vector2(Mathf.Cos(45) * ballImpulse, 0f));
 
         Vector2 direction = new Vector2(Random.Range(135, 225), transform.position.y);
         rb2d.AddForce(-direction * ballImpulse);
 
-	}
+    }
 	
 	/// <summary>
 	/// Update is called once per frame
